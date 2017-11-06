65c5fb0c7571a477edb80a0c5aa04e238e42e8a9
raphatapi <raphaeltapioca10@gmail.com>
2017-11-06 00:01:38 -0700

    github authentication

--- src/book.js
+++ src/book.js
@@ -2,7 +2,48 @@ var selectedStory;
 var selectedChoice;
 var currentStory = 0;
 var currentPage = "pageStart";
-var json = stories[currentStory].pageStart.nextPage;
+
+// FIREBASE AUTHENTICATION
+// var config = {
+//         apiKey: "AIzaSyAWUtB7pvGbWyUCdRJl0cLuf2_Ln0dWI5A",
+//         authDomain: "purple-tree-5f62c.firebaseapp.com",
+//         databaseURL: "https://purple-tree-5f62c.firebaseio.com",
+//         projectId: "purple-tree-5f62c",
+//         storageBucket: "",
+//         messagingSenderId: "699925334051"
+//       };
+//       firebase.initializeApp(config);
+
+      var provider = new firebase.auth.GithubAuthProvider();
+
+        function githubSignin() {
+           firebase.auth().signInWithPopup(provider)
+           
+           .then(function(result) {
+              var token = result.credential.accessToken;
+              var user = result.user;
+                
+              console.log(token)
+              console.log(user)
+           }).catch(function(error) {
+              var errorCode = error.code;
+              var errorMessage = error.message;
+                
+              console.log(error.code)
+              console.log(error.message)
+           });
+        }
+
+        function githubSignout(){
+           firebase.auth().signOut()
+           
+           .then(function() {
+              console.log('Signout successful!')
+           }, function(error) {
+              console.log('Signout failed')
+           });
+        }
+
 
 $(document).ready(function() {
     
@@ -28,8 +69,16 @@ function mainHTML() {
 		}
 	};
 
+// GIPHY API FOR IMAGES
+// function getImage() {
+//     var topic = stories[currentStory][currentPage].topic;
+//     var queryURL = "https://api.giphy.com/v1/gifs/random?q=" + topic + "&api_key=6xfaAb1rplRM55HrAsCdiL6jn7DwEZLy&limit=10"
+// }
+
 function storyHTML(currentPage) {
-	var storyHTML = "<img class='center-block' src='images/ouat.gif'><h1>" + stories[currentStory].storyTitle + "</h1>";
+
+
+	var storyHTML = "<h1>" + stories[currentStory].storyTitle + "</h1>";
     $(".main-area").html(storyHTML); 
     var storyImg = $("<img>");
     storyImg.addClass("inline-block img-story");
@@ -42,35 +91,19 @@ function storyHTML(currentPage) {
             //display the key and value pair
             var choiceBtn = $("<button>");
             choiceBtn.addClass("text-center btn btn-warning btn-lg choice");
-            // choiceBtn.attr("id", k);
             choiceBtn.text(v);
             choiceBtn.on("click", function(event){
-                // selectedChoice = k;
-                // console.log(selectedChoice);
                 choiceUpdate(k);
             })
             $(".main-area").append(choiceBtn);
 
-            // $(".choice").on("click", function(event){
-            //     selectedChoice = $(this).attr("id");
-            //     // storyHTML(selectedChoice);
-            //     console.log(selectedChoice);
-            //     // if (selectedChoice === v) {
-            //     //     console.log(selectedChoice);
-            //     //     choiceA();
-            //     // } else if (selectedChoice === v) {
-            //     //     console.log(selectedChoice);
-            //     //     choiceB();
-                    
-            //     // };
-            // });
         });        
 
     
 };
 
 function choiceUpdate(choice){
-    console.log(choice);
+    // console.log(choice);
     if (choice.indexOf("Story") > -1){
         if (choice.indexOf("Another") > -1){
             location.reload();
